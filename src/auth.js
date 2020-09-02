e5f5536d38416f587314e80e53f76ca65075fb3b
Kevin Danikowski <kdaniko@lowes.com>
2020-09-02 08:27:26 -0500

    feat(local storage): add local storage for auth so can navigate between pages

--- src/auth.js
+++ src/auth.js
@@ -12,19 +12,20 @@ import { BaseTokenRequestHandler } from "@openid/appauth/built/token_request_han
 import { FetchRequestor } from "@openid/appauth/built/xhr";
 import { RevokeTokenRequest } from "@openid/appauth/built/revoke_token_request";
 
+const AUTH_TOKEN = 'appauth_token'
+
 const requestor = new FetchRequestor();
 
 /* an example open id connect provider */
 const openIdConnectUrl = "https://accounts.google.com";
 
 /* example client configuration */
-const clientId =
-  "511828570984-7nmej36h9j2tebiqmpqh835naet4vci4.apps.googleusercontent.com";
-const redirectUri = "http://localhost:8000/app/redirect.html";
+const clientId = "511828570984-7nmej36h9j2tebiqmpqh835naet4vci4.apps.googleusercontent.com";
+const redirectUri = "http://localhost:8000/token";
 const scope = "openid";
 
-export default class Auth {
-  constructor(snackbar) {
+class Auth {
+  constructor() {
     this.notifier = new AuthorizationNotifier();
     this.authorizationHandler = new RedirectRequestHandler();
     this.tokenHandler = new BaseTokenRequestHandler(requestor);
@@ -32,32 +33,73 @@ export default class Auth {
     this.authorizationHandler.setAuthorizationNotifier(this.notifier);
     // set a listener to listen for authorization responses
     this.notifier.setAuthorizationListener((request, response, error) => {
-      log("Authorization request complete ", request, response, error);
       if (response) {
         this.request = request;
         this.response = response;
+        // authorization code to make token requests
+        // we don't save in local storage so that we require
+        // login once tokens expire
         this.code = response.code;
-        log(`Authorization Code ${response.code}`);
       }
     });
   }
 
-  isAuthorizedUser() {
-    return !!this.code;
+  // get from local storage
+  getToken() {
+    const tokenStorage = localStorage.getItem(AUTH_TOKEN)
+    const token = typeof tokenStorage === 'string' ? JSON.parse(tokenStorage) : null
+
+    if (
+      token && 
+      token.tokenType && 
+      token.scope && 
+      token.accessToken && 
+      !isNaN(token.issuedAt) && 
+      !isNaN(token.expiresIn)
+    ) {
+      const now = new Date().getTime()
+      const timeLeft = token.issuedAt + token.expiresIn - now / 1000
+      const isStillValid = timeLeft > 0
+
+      if (isStillValid) {
+        return token
+      }
+    }
+
+    return null
   }
 
+  // set from local storage
+  setToken(token) {
+    return localStorage.setItem(AUTH_TOKEN, JSON.stringify(token))
+  }
+
+  logger(...args) {
+    const shouldLog = true
+    if (shouldLog) {
+      log(...args)
+    }
+  }
+
+  // authorized if has auth code or access token
+  isAuthorizedUser() {
+    return !!this.code || !!this.getToken();
+  }
+
+  // revokes the current token
   signOut() {
     if (!this.configuration) {
-      log("Please fetch service configuration.");
+      this.logger("Please fetch service configuration.");
       return;
     }
 
     let request = null;
-    if (this.tokenResponse) {
+    const token = this.getToken()
+    if (token) {
       // use the token response to make a request for an access token
       request = new RevokeTokenRequest({
         client_id: clientId,
-        token: this.tokenResponse.accessToken,
+        token: token.accessToken,
       });
     }
 
@@ -66,24 +108,12 @@ export default class Auth {
         .performRevokeTokenRequest(this.configuration, request)
         .then((response) => {
           this.code = undefined;
-          this.tokenResponse = undefined;
-
-          log(`Logged out.`);
+          localStorage.removeItem(AUTH_TOKEN)
         })
         .catch((error) => {
-          log("Something bad happened", error);
+          this.logger("Issue revoking token", error);
         });
     }
-  }
-
-  hasToken() {
-    log(
-      "Has token:",
-      Boolean(this.tokenResponse && !!this.tokenResponse.accessToken),
-      ", Token response: ",
-      this.tokenResponse
-    );
-    return Boolean(this.tokenResponse && !!this.tokenResponse.accessToken);
   }
 
   fetchServiceConfiguration() {
@@ -92,17 +122,16 @@ export default class Auth {
       requestor
     )
       .then((response) => {
-        log("Fetched service configuration", response);
         this.configuration = response;
       })
       .catch((error) => {
-        log("Something bad happened", error);
+        this.logger("Problem fetching service config", error);
       });
   }
 
   makeAuthorizationRequest() {
     // create a request
-    let request = new AuthorizationRequest({
+    const request = new AuthorizationRequest({
       client_id: clientId,
       redirect_uri: redirectUri,
       scope: scope,
@@ -117,21 +146,38 @@ export default class Auth {
         request
       );
     } else {
-      log(
+      this.logger(
         "Fetch Authorization Service configuration, before you make the authorization request."
       );
     }
   }
 
+  refreshToken() {
+    return this.fetchServiceConfiguration().then(() => this.makeTokenRequest());
+  }
+
+  // gets or refreshes token
   makeTokenRequest() {
     if (!this.configuration) {
-      log("Please fetch service configuration.");
+      this.logger("Please fetch service configuration.");
       return;
     }
 
     let request = null;
-    if (this.code) {
+    const token = this.getToken()
+    if (token && token.refreshToken) {
+      // use the token response to make a request for an access token
+      request = new TokenRequest({
+        client_id: clientId,
+        redirect_uri: redirectUri,
+        grant_type: GRANT_TYPE_REFRESH_TOKEN,
+        code: undefined,
+        refresh_token: token.refreshToken,
+        extras: undefined,
+      });
+    } else if (this.code) {
       let extras = undefined;
+      // initial code request, for PKCE we need code_verifier challange
       if (this.request && this.request.internal) {
         extras = {};
         extras["code_verifier"] = this.request.internal["code_verifier"];
@@ -143,17 +189,7 @@ export default class Auth {
         grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
         code: this.code,
         refresh_token: undefined,
-        extras: extras,
-      });
-    } else if (this.tokenResponse) {
-      // use the token response to make a request for an access token
-      request = new TokenRequest({
-        client_id: clientId,
-        redirect_uri: redirectUri,
-        grant_type: GRANT_TYPE_REFRESH_TOKEN,
-        code: undefined,
-        refresh_token: this.tokenResponse.refreshToken,
-        extras: undefined,
+        extras,
       });
     }
 
@@ -161,34 +197,26 @@ export default class Auth {
       return this.tokenHandler
         .performTokenRequest(this.configuration, request)
         .then((response) => {
-          let isFirstRequest = false;
-          if (this.tokenResponse) {
-            // copy over new fields
-            this.tokenResponse.accessToken = response.accessToken;
-            this.tokenResponse.issuedAt = response.issuedAt;
-            this.tokenResponse.expiresIn = response.expiresIn;
-            this.tokenResponse.tokenType = response.tokenType;
-            this.tokenResponse.scope = response.scope;
-          } else {
-            isFirstRequest = true;
-            this.tokenResponse = response;
-          }
-
-          // unset code, so we can do refresh token exchanges subsequently
-          this.code = undefined;
-          if (isFirstRequest) {
-            log(`Obtained a refresh token ${response.refreshToken}`);
-          } else {
-            log(`Obtained an access token ${response.accessToken}.`);
-          }
+          this.setToken(response)
         })
         .catch((error) => {
-          log("Something bad happened", error);
+          this.logger("Failed to perform token request", error);
         });
+    } else {
+      this.logger("Error: Token request requires auth code or refresh token.");
     }
+  }
+
+  signIn() {
+    return this.fetchServiceConfiguration().then(() => this.makeAuthorizationRequest());
   }
 
   checkForAuthorizationResponse() {
     return this.authorizationHandler.completeAuthorizationRequestIfPossible();
   }
 }
+
+// keep in one instance so don't lose this.code value
+const auth = new Auth()
+
+export default auth
